4c07a4705f192f03251437dafd1ced6b1cdb6adc
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-05-29 16:25:46 -0400

    Make input node lists vectors

--- src/TriggerClusterMaker.h
+++ src/TriggerClusterMaker.h
@@ -48,19 +48,16 @@ struct TriggerClusterMakerConfig {
   std::string outFileName = "test.root";
 
   // input nodes
-  std::array<std::string, 2> inLL1Nodes = {
-    "LL1OUT_RAW_JET",
+  std::vector<std::string> inLL1Nodes = {
     "LL1OUT_JET"
   };
-  std::array<std::string, 8> inPrimNodes = {
-    "TRIGGERPRIMITIVES_RAW_EMCAL",
-    "TRIGGERPRIMITIVES_RAW_EMCAL_LL1",
-    "TRIGGERPRIMITIVES_RAW_JET",
+  std::vector<std::string> inPrimNodes = {
+    "TRIGGERPRIMITIVES_JET",
     "TRIGGERPRIMITIVES_EMCAL",
     "TRIGGERPRIMITIVES_EMCAL_LL1",
-    "TRIGGERPRIMITIVES_HCALIN",
     "TRIGGERPRIMITIVES_HCALOUT",
-    "TRIGGERPRIMITIVES_HCAL_LL1"
+    "TRIGGERPRIMITIVES_HCAL_LL1",
+    "TRIGGERPRIMITIVES_HCALIN"
   };
 
 };
